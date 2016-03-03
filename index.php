6d4d6194bbdfc10822c8843240075207e65f7d52
david carbajal criollo <riccc.jjjhhggg@gmail.com>
2016-03-03 12:13:33 -0500

    trabajo002

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>david carbajal criollo</title>
+	<link rel="stylesheet" href="estylos.css">
+</head>
+<body>
+	<div class="contenedor">
+	<nav>
+		<ul class="menu-lista">
+			<li class="menu-item"><a href="" title="" class="menu-link">INICIO</a></li>
+			<li class="menu-item"><a href="" title="" class="menu-link">PRODUCTOS</a></li>
+			<li class="menu-item"><a href="" title="" class="menu-link">PORTAFOLIOS</a></li>
+			<li class="menu-item"><a href="" title="" class="menu-link">NOSOTROS</a></li>
+			<li class="menu-item"><a href="" title="" class="menu-link">CONTACTENOS<a/></li>
+		</ul>
+	</nav>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+	<h1></h1>
+</div class="slider">
+<img src="1.jpg" height="600" width="1350">
+<img src="2.jpe" height="200" width="330">
+<img src="3.gif" height="200" width="330">
+<img src="4.jpg" height="200" width="330">
+<img src="5.gif" height="200" width="330">
+
+
+</body>
+</html>
